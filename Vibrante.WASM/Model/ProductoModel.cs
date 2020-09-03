682c69a49aa11aff6d4effae59125e6dbb8fda86
Eliseo <eliseo_rodriguez@latisinformatica.com>
2020-09-02 23:02:35 -0400

    Add project files.

---
+++ Vibrante.WASM/Model/ProductoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vibrante.WASM.Model
+{
+    public class ProductoModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string UrlFoto { get; set; }
+    }
+}
